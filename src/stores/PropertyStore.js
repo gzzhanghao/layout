33f04be1631831745dc27b11f33fa18ccc210aa8
Jason <jason.daurus@gmail.com>
2015-12-02 18:42:03 +0800

    Add content panel

--- src/stores/PropertyStore.js
+++ src/stores/PropertyStore.js
@@ -4,6 +4,7 @@ import Store from './Store'
 import LayoutPanel from '../components/properties/LayoutPanel'
 import BorderPanel from '../components/properties/BorderPanel'
 import SourcePanel from '../components/properties/SourcePanel'
+import ContentPanel from '../components/properties/ContentPanel'
 import BackgroundPanel from '../components/properties/BackgroundPanel'
 
 class PropertyStore extends Store {
@@ -14,6 +15,7 @@ class PropertyStore extends Store {
         { name: 'Layout', key: 'layout', panel: LayoutPanel, available: /./ },
         { name: 'Border', key: 'border', panel: BorderPanel, available: /./ },
         { name: 'Background', key: 'background', panel: BackgroundPanel, available: /./ },
+        { name: 'Content', key: 'content', panel: ContentPanel, available: /^(text)$/ },
         { name: 'Source', key: 'source', panel: SourcePanel, available: /^(image)$/ }
       ]
     }))
